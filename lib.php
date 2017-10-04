40d06f97fb486b436bc2257488832a769b8222ff
jjupin <jonathan.jupin@univ-lemans.fr>
2017-10-04 17:49:18 +0200

    BUUUUUG : location du thème en PROD, maintenant il ne devrait plus y avoir de souci !!!!

--- lib.php
+++ lib.php
@@ -57,9 +57,9 @@ function theme_eadumboost_get_main_scss_content($theme) {
 	}
 
 	// Pre CSS - this is loaded AFTER any prescss from the setting but before the main scss.
-	$pre = file_get_contents($CFG->dirroot . '/theme/eadumboost/scss/pre.scss');
+	$pre = file_get_contents($CFG->themedir . '/eadumboost/scss/pre.scss');
 	// Post CSS - this is loaded AFTER the main scss but before the extra scss from the setting.
-	$post = file_get_contents($CFG->dirroot . '/theme/eadumboost/scss/post.scss');
+	$post = file_get_contents($CFG->themedir . '/eadumboost/scss/post.scss');
 
 	// Combine them together.
 	return $pre . "\n" . $scss . "\n" . $post;
