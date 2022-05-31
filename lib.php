ab86e75f7fabc4bd8974c0f20a5e4f7b7c0a237b
jonathanjoegit <12695763+jonathanjoegit@users.noreply.github.com>
2022-05-31 17:06:22 +0200

    test push

--- lib.php
+++ lib.php
@@ -18,7 +18,7 @@
  * Theme functions.
  *
  * @package    theme_valeoboost
- * @copyright  2022 Jonathan J. - Le Mans Université
+ * @copyright  2021 Jonathan J. - Le Mans Université
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
